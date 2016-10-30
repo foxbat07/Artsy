45fde36c6200b642a30c158702b2c4586ddf3947
mohit.hingorani <mohit.hingorani@umail.ucsb.edu>
2016-10-30 13:59:15 -0700

    fix bugs

--- WatchMe/sketch.js
+++ WatchMe/sketch.js
@@ -2,14 +2,15 @@ var centerX,centerY;
 var radius = 600;
 var eStroke = 80;
 var nStroke = 100;
+var c;
 
 
 
 function setup() {
-	 createCanvas(windowWidth, windowHeight);
+	 c = createCanvas(windowWidth, windowHeight);
 	 centerX = windowWidth/2;
 	 centerY = windowHeight/2;
-	 frameRate(60);
+	 frameRate(1);
 
 }
 
@@ -25,13 +26,17 @@ function draw() {
 
   var sec = TWO_PI * second() / 60;
   var min = TWO_PI * minute() / 60;
-  var hr = TWO_PI * ( hour() < 12 ? hour() : hour() - 12) / 12;
-  
+  //var hr =  TWO_PI *  12/ 12;
+  var hr = TWO_PI * ( hour() < 12 ? hour() : hour() + 12) / 12 ;
+  if(hr == 0 || hr == TWO_PI )
+  {
+  	hr = 0.0001;		//workaround for time bug
+  }
   drawArc( radius, cs, sec ,60);
   drawArc( radius- nStroke, cm, min ,60);
-  drawArc( radius - 2*nStroke, ch, hr, 12 ); 
+  drawArc( radius - 2*nStroke, ch , hr , 12 ); 
 
-  
+  console.log("time: "+ hour()+"  "+ minute()+"  "+ second()  +"   " + hr+" "+min+" "+sec);
 }
 
 //radius,color,angle based on time
@@ -40,7 +45,7 @@ function draw() {
 function drawArc(  r,  c,  angle, div ) {
   
   //var w = color('rgb(255,255,255)');
-  var grey1 = color(15);
+  var grey1 = color(20);
   var grey2 = color(0);
 
   //outer shadow
@@ -49,13 +54,10 @@ function drawArc(  r,  c,  angle, div ) {
 
   //actual arc
   fill(c);
-  arc(centerX,centerY,r,r,-PI/2,-PI/2 + angle);
+  arc(centerX,centerY,r,r,-PI/2, -PI/2 + angle);
 
-  //draw better grids h =12, m= 60 ,s = 60;
-  fill(c);
-  arc(centerX,centerY,r,r,-PI/2,-PI/2 + angle);
-
-  //make white
+  //draw better grids h =12, m= 60 ,s = 60; 
+  // //make white
   for ( var i = 0 ; i < 2* PI ; i += PI/ (div/2) )
   	  {
   	  	fill(grey1);
@@ -65,7 +67,7 @@ function drawArc(  r,  c,  angle, div ) {
   //inner clean
   fill(grey2);
   ellipse(centerX,centerY,r-eStroke,r-eStroke);
-  
+
   
 
 }
@@ -73,10 +75,7 @@ function drawArc(  r,  c,  angle, div ) {
 
 
 function mousePressed() {
-  saveFrames("out", "png", 1, 60, function(data){
-   console.log("iamge saved");
-
-  });
+   //saveCanvas(c, 'myCanvas', '.png');
 }
 
 
