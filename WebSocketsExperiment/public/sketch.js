1fe25f527ff30db3ffeed343d30423d1611d6f92
mohit.hingorani <mohit.hingorani@umail.ucsb.edu>
2016-12-01 00:08:50 -0800

    updating multi draw

--- WebSocketsExperiment/public/sketch.js
+++ WebSocketsExperiment/public/sketch.js
@@ -1,9 +1,30 @@
+var socket;
+
 function setup() {
 createCanvas(1200,800);
 
+socket = io.connect('http://localhost:3010');  //127.0.0.1
+socket.on('mouse',newDrawing);
+
 }
 
+function newDrawing(data){
+	 fill('red');
+	 ellipse(data.x,data.y,20,20);
+}
 function draw() {
   
-  ellipse(mouseX,mouseY,40,40);
+  
+
+}
+
+function mouseDragged() {
+	console.log(mouseX,mouseY);
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+	socket.emit('mouse',data);
+	fill('white');
+	ellipse(mouseX,mouseY,40,40);
 }
--- WebSocketsExperiment/server.js
+++ WebSocketsExperiment/server.js
@@ -6,7 +6,21 @@ var server = app.listen(3010);
 
 var io = socket(server);
 
-io.on('connection');
+io.sockets.on('connection',newConnection);
+
+function newConnection(socket){
+	console.log('new connection: '+ socket.id);
+
+	socket.on('mouse',mouseMessage);
+
+
+	function mouseMessage(data){
+
+		socket.broadcast.emit('mouse',data);
+		console.log(data);
+
+	}
+}
 
 app.use(express.static('public'));
 console.log("server is now running");
